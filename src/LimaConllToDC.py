3fde6bcc6873f8871e31291aaaf82ccd66e3b5ef
Anandou C <candassamy.anandou@gmail.com>
2020-02-27 12:05:46 +0000

    stnfd and nltk univ files

--- src/LimaConllToDC.py
+++ src/LimaConllToDC.py
@@ -3,15 +3,15 @@ import os
 
 os.chdir("..");
 
-#Récupération des arguments
+#Recuperation des arguments
 if(len(sys.argv) < 3):
   print("Not enough arguments. GetTextFromTagged.py take two arguments");
   print("Try with: python3 GetTextFromTagged.py <inputFile> <outputFile>");
-#Stockage des chemins d'entrée et de sortie
+#Stockage des chemins d'entree et de sortie
 inputPath = sys.argv[1];
 outputPath = sys.argv[2];
 
-#récupération du contenu du fichier d'entrée
+#recueperation du contenu du fichier d'entree
 taggedFile = open(inputPath, "r+");
 taggedContent = taggedFile.read();
 taggedFile.close();
@@ -28,8 +28,10 @@ for line in inputLines:
       #extraction du mot et de son tag
       lineColumn = line.split("\t");
       rsltStr += lineColumn[1] + "\t" + lineColumn[3] + "\n";
+  else:
+    rsltStr += "\n";
 
-#écriture dans fichier de sortie
+#ecriture dans fichier de sortie
 outputFile = open(outputPath, "w+");
 outputFile.write(rsltStr);
 outputFile.close();
